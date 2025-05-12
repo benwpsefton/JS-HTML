a754450d04c39395a6358bd88dcfaa6c8aa6929a
benwpsefton <benwpsefton@gmail.com>
2025-05-12 10:34:20 +0800

    renamed and started AT1.2

---
+++ AT1.2/Part 2 - Boxes/js/handling.js
@@ -0,0 +1,37 @@
+// JavaScript Assessment 1: Task 2 - Event Handling
+
+//No need for globals here
+
+
+
+functio ??() {//Last button function here
+alert("Don't forget to use strict in Function Name here");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+foutncin() {//Fisrt button function here
+alert("Don't forget to use strict in Function Name here");
+}
+
+function init(){
+alert("Don't forget to use strict in init function");
+
+
+// All event listeners here
+
+
+}
+
+
+window.onload = init;
